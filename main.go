ee8a831e3c37d3288bd2b3e521cfaabfb1c289f2
Alpharii <alphari404@gmail.com>
2025-01-26 14:40:49 +0700

    add cors in main

--- main.go
+++ main.go
@@ -4,18 +4,38 @@ import (
 	"backend-go/config"
 	"backend-go/routes"
 	"log"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err:= godotenv.Load(); err != nil{
+	// Load .env file
+	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	// Connect to the database
 	config.ConnectDB()
 
-	r:= gin.Default()
+	// Initialize Gin router
+	r := gin.Default()
+
+	// Setup CORS
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173", "https://example.com"}, // Ganti dengan domain asal yang diizinkan
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour, // Cache hasil preflight request selama 12 jam
+	}))
+
+	// Initialize routes
 	r = routes.InitRouter()
+
+	// Run the server
 	r.Run(":8080")
 }
